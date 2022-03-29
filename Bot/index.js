78d515987c3f531e801b38b608ae22d238a0e1e4
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-03-29 23:29:05 +0200

    added env dependency

--- Bot/index.js
+++ Bot/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const Web3 = require('web3')
 let web3
 
 const main = async () => {
-    let account = '0x975CE1D285B334b415bc7FC4B676f229aA904c37'
-    // web3 = new Web3('http://127.0.0.1:7545')
-    // web3 = new Web3('http://host.docker.internal:7545')
-    web3 = new Web3('http://ganache:7545')
+    var web3Url = process.env["BLOCKCHAINURL"];
+
+    let account = '0x975CE1D285B334b415bc7FC4B676f229aA904c37'    
+    web3 = new Web3(web3Url)
     let balance = await web3.eth.getBalance(account)
 
     console.log(`Balance: ${balance}`)
